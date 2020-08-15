be856a4f762b9910e93ce3650471c1df41795984
NickGaultney <nmgaultney@gmail.com>
2020-08-14 20:54:58 -0600

    Remove API Authentication

--- app/controllers/api/v1/bot_controller.rb
+++ app/controllers/api/v1/bot_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::BotController < ApplicationController
 	skip_before_action :verify_authenticity_token
-	
+
 	def github_invite
-		render json: system("python3 /home/CaptainWhymer/github_invite.py #{params[username]}")
+		invite_sent = system("python3 /home/CaptainWhymer/github_invite.py #{params[username]}")
+		render json: invite_sent
 	end
 end
