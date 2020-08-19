d486b9a9f188666b876bf911b4d40a7690e08bdb
NickGaultney <nmgaultney@gmail.com>
2020-08-19 14:38:29 -0500

    Threading

--- app/controllers/api/v1/bot_controller.rb
+++ app/controllers/api/v1/bot_controller.rb
@@ -1,8 +1,11 @@
+require 'open3'
+
 class Api::V1::BotController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
 	def github_invite
-		invite_sent = system("python3 /home/CaptainWhymer/github_invite.py #{params[username]}")
-		render json: invite_sent
+		Open3.popen2("python3 /home/CaptainWhymer/github_invite.py #{params[username]}") do |i, o, t|
+			p o.each { |line| puts line; sleep(5) }
+		end
 	end
 end
