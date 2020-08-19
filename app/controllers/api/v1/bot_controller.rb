433abc88188839c055a1d86bfd0674d33aaadec9
NickGaultney <nmgaultney@gmail.com>
2020-08-19 14:48:39 -0500

    Threading

--- app/controllers/api/v1/bot_controller.rb
+++ app/controllers/api/v1/bot_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::BotController < ApplicationController
 
 	def github_invite
 		Open3.popen2("python3 /home/CaptainWhymer/github_invite.py #{params[username]}") do |i, o, t|
-			p o.each { |line| puts line; sleep(5) }
+			p o.each { |line| Rails.logger.info line; sleep(5) }
 		end
 	end
 end
