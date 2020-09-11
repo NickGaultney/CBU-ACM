24b01f5e8baeabf896108cc4114a06f8646690d1
Nick Mark <nmgaultney@gmail.com>
2020-09-11 13:51:00 +0000

    Revert "Dark Welcome"
    This reverts commit 0b3a905f79075d84593ed4c75a9b42528d245099.

--- app/controllers/api/v1/bot_controller.rb
+++ app/controllers/api/v1/bot_controller.rb
@@ -9,7 +9,6 @@ class Api::V1::BotController < ApplicationController
 		Open3.popen2("python3 /home/CaptainWhymer/github_invite.py #{params[:username]}") do |i, o, t|
 			p o.each { |line| Rails.logger.info line }
 		end
-		
-		render :json "Success!"
+		Rails.logger.info "*** MADE IT HERE ***"
 	end
 end
